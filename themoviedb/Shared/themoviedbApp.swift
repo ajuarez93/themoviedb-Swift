c88c4e8a91d4e377eff0232c91aa41685aa3d7d5
Alfonso Juarez <joses.guerreros@gmail.com>
2022-08-29 23:48:59 -0500

    Init

---
+++ themoviedb/Shared/themoviedbApp.swift
@@ -0,0 +1,17 @@
+//
+//  themoviedbApp.swift
+//  Shared
+//
+//  Created by Jose Alfonso Guerrero Juarez on 29/8/22.
+//
+
+import SwiftUI
+
+@main
+struct themoviedbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
